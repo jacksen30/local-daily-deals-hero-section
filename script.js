bda3771053776331c3f0b7aaeeb95059f33e4632
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-10-31 21:02:37 +0800

    fix meta viewport tag typo, experiment with 100% vh hamburger menu on open

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // JavaScript to toggle nav-list on mobile
 
 function toggleMenu() {
+    const header = document.getElementById("header");
     const navList = document.getElementById("nav-list");
     const headerBtn = document.getElementById("header-btn");
     if (navList.style.display === "none" || navList.style.display === "") {
         navList.style.display = "block";
         headerBtn.style.display = "none";
+        header.style.backgroundColor = "#16AA2D";
+        header.style.height = "100vh";
     } else {
         navList.style.display = "none";
         headerBtn.style.display = "block";
+        header.style.backgroundColor = "white";
+        header.style.height = "";
     }
 }
 
